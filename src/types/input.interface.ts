b9a9d7b3f1aaefa60d54e6c0ca8e86924b866363
popochoo <bestekst0@gmail.com>
2026-06-12 19:19:16 +0300

    update input.interface.ts

--- src/types/input.interface.ts
+++ src/types/input.interface.ts
@@ -1,3 +1,11 @@
 export interface InputProps {
     addTask: (title: string) => void;
 }
+
+export interface InputTaskProps {
+    id: string
+    title: string
+    onDone: (title: string) => void
+    onEdited: (id: string, value: string) => void
+    onRemoved: (id: string) => void
+}
